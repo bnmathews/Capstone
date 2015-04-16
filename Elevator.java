bedf797c744680618cd5ed218b742c16710dc892
Ben Mathews <jahnsenn@gmail.com>
2015-04-16 13:09:07 -0500

    More insertion + room sizes

--- Elevator.java
+++ Elevator.java
@@ -4,14 +4,6 @@ public class Elevator extends Room
     private String name;
     public Elevator()
     {
-        super("e");
-    }
-    
-    public void constructElevator(Graphics page, int startX, int startY)
-    {
-        page.drawLine (startX+10, 20, 20, 20);
-        page.drawLine (startX+10, 50, 20, 50);
-        page.drawLine (20, 20, 20, 50);
-        page.drawLine (50, 20, 50, 50);
+        super("e",20,30);
     }
 }
