a8f44ba223eebcc8d17e5321b3926ecf10a4b76f
Ben Mathews <jahnsenn@gmail.com>
2015-04-17 12:02:24 -0500

    Revert "Revert "More insertion + room sizes""
    This reverts commit 49a44de22dcff31bf229da4796afffb08c3695bf.

--- Room.java
+++ Room.java
@@ -2,22 +2,42 @@ import java.awt.*;
 public class Room
 {
     private String name;
+    private int width = 30;
+    private int height = 30;
     public Room()
     {
         name = "r";
     }
     
-    public Room(String n)
+    public Room(String n, int w, int h)
     {
         name = n;
+        width = w;
+        height = h;
     }
     
-    public void constructRoom(Graphics page, int increase, int heightIncrease)
+    public int getWidth()
     {
-        page.drawLine (20+increase, 20+heightIncrease, 50+increase, 20+heightIncrease);
-        page.drawLine (20+increase, 50+heightIncrease, 50+increase, 50+heightIncrease);
-        page.drawLine (20+increase, 20+heightIncrease, 20+increase, 50+heightIncrease);
-        page.drawLine (50+increase, 20+heightIncrease, 50+increase, 50+heightIncrease);
+        return width;
+    }
+    
+    public int getHeight()
+    {
+        return height;
+    }
+    
+    public void constructRoom(Graphics page, int startingX, int startingY, int increase, int heightIncrease)
+    {
+        /*
+        page.drawLine (20+increase, 20+heightIncrease, 50+increase, 20+heightIncrease); //draw top line
+        page.drawLine (20+increase, 20+heightIncrease, 20+increase, 50+heightIncrease); //draw left line
+        page.drawLine (50+increase, 20+heightIncrease, 50+increase, 50+heightIncrease); //draw right line
+        page.drawLine (20+increase, 50+heightIncrease, 50+increase, 50+heightIncrease); //draw bottom line
+        */
+        page.drawLine (startingX+increase, startingY+heightIncrease, startingX+width+increase, startingY+heightIncrease); //draw top line
+        page.drawLine (startingX+increase, startingY+heightIncrease, startingX+increase, startingY+height+heightIncrease); //draw left line
+        page.drawLine (startingX+width+increase, startingY+heightIncrease, startingX+width+increase, startingY+height+heightIncrease); //draw right line
+        page.drawLine (startingX+increase, startingY+height+heightIncrease, startingX+width+increase, startingY+height+heightIncrease); //draw bottom line
     }
     
     public String getName()
